1170d833fe81e435c5a42c41e4f647f945895fcd
jastikka-03 <jastikka.p03@gmail.com>
2023-02-11 14:34:52 +0530

    buttoncolorchangehovor

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+function mouseOver(button)
+{
+    document.getElementById("button").style.backgroundColor="#336666";
+}
+function mouseOut(button){
+    document.getElementById("button").style.backgroundColor="#669999";
+}
